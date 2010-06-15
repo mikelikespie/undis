7121d5f9077173adfa5883a562d04e789421f67b
Mike Lewis <mikelikespie@gmail.com>
2010-06-15 02:34:07 -0700

    Wee

--- undis.go
+++ undis.go
@@ -6,6 +6,7 @@ import (
 	"net"
 	"os"
 	"bufio"
+	"strconv"
 )
 
 const (
@@ -50,47 +51,123 @@ func readCmd(in net.Conn) int {
 }
 
 
+//Reads an integer, then reads some bytes
+func parseBulk(reader *bufio.Reader) []byte {
 
-// Takes the stream after the * has been read
-func parseMulti(reader *bufio.Reader) [][]byte {
-	return [][]byte{}
+	// read the expected $
+	b, err := reader.ReadByte()
+	if b != '$' || err != nil {
+		fmt.Fprintf(os.Stderr, "Format not expected: %v\n", err)
+		panic("oh no")
+		return nil
+	}
+
+	line, err := reader.ReadString('\n')
+	if err != nil || line[len(line)-2] != '\r' {
+		fmt.Fprintf(os.Stderr, "Format not expected: %v\n", err)
+		panic("oh no")
+		return nil
+	}
+
+	line = line[0 : len(line)-2]
+
+	numBytes, err := strconv.Atoi(line)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Format not expected: %v\n", err)
+		panic("oh no")
+		return nil
+	}
+
+	buff := make([]byte, numBytes)
+
+	nn, err := reader.Read(buff)
+
+	if nn != len(buff) || err != nil {
+		fmt.Fprintf(os.Stderr, "Format not expected: %v\n", err)
+		panic("oh no")
+		return nil
+	}
+
+	//seek to the end of the line
+	reader.ReadByte()
+	reader.ReadByte()
+
+	return buff
 }
 
-func parseSingle(reader *bufio.Reader) [][]byte {
+// Takes the stream after the * has been read
+func parseMulti(reader *bufio.Reader) (string, [][]byte) {
+	print("Got multi \n")
+
+	line, err := reader.ReadString('\n')
+
+	if err != nil || line[len(line)-2] != '\r' {
+		panic("oh no")
+		fmt.Fprintf(os.Stderr, "Format not expected: %v\n", err)
+		return "", nil
+	}
+
+
+	line = line[0 : len(line)-2] // trim off the \r
+
+
+	numArgs, err := strconv.Atoi(line)
+	if err != nil {
+		panic("oh no")
+		fmt.Fprintf(os.Stderr, "ohFormat not expected: %v\n", err)
+		return "", nil
+	} else if  numArgs < 1 {
+		panic("oh no")
+		fmt.Fprintf(os.Stderr, "bad format thought hwe'd have more than %d args\n", numArgs)
+		return "", nil
+
+	}
+
+	numArgs--; // Subtract one off because we're going to be parsing the cmd Name
+
+	cmdName := string(parseBulk(reader))
+
+
+	return cmdName, [][]byte{}
+}
+
+func parseSingle(reader *bufio.Reader) (string, [][]byte) {
 
 	cmdName, err := reader.ReadString(' ')
 
-	if (err != nil) {
+	if err != nil {
+		panic("oh no")
 		fmt.Fprintf(os.Stderr, "Format not expected: %v\n", err)
-		return nil
+		return "", nil
 	}
-
 
 	fmt.Printf("Got cmd '%s\n'", cmdName)
 
-	isBulk := cmds[cmdName] & REDIS_CMD_BULK == REDIS_CMD_BULK
+	isBulk := cmds[cmdName]&REDIS_CMD_BULK == REDIS_CMD_BULK
 
 	line, err := reader.ReadString('\n')
-	if (err != nil || line[len(line)-1] != '\r') {
+	if err != nil || line[len(line)-2] != '\r' {
 		fmt.Fprintf(os.Stderr, "Format not expected: %v\n", err)
-		return nil
+		return "", nil
 	}
 
 	// Trim the last character
-	line = line[0:len(line)-1]
+	line = line[0 : len(line)-2]
 
-	fmt.Printf("Got rest of args '%s\n' isBulk '%v'", line, isBulk)
+	fmt.Printf("Got rest of args '%s\n' isBulk '%v'\n", line, isBulk)
 
 	// The line should end with crlf so check the last char is \r
 
-	return [][]byte{}
+	return cmdName, [][]byte{}
 }
 
 
 // proxies from client to server
 func inLoop(inReader *bufio.Reader, outWriter *bufio.Writer) {
+	print("starting in loop\n")
 	for {
-		firstChar, err := inReader.ReadByte();
+		firstChar, err := inReader.ReadByte()
+		var cmd string
 
 		if err != nil {
 			fmt.Fprintf(os.Stderr, "Format not expected: %v\n", err)
@@ -100,11 +177,13 @@ func inLoop(inReader *bufio.Reader, outWriter *bufio.Writer) {
 		var cmdArgs [][]byte
 
 		if firstChar == '*' {
-			cmdArgs = parseMulti(inReader)
+			cmd, cmdArgs = parseMulti(inReader)
 		} else {
-			inReader.UnreadByte();
-			cmdArgs = parseSingle(inReader)
+			inReader.UnreadByte()
+			cmd, cmdArgs = parseSingle(inReader)
 		}
+
+		print("Got command " + cmd + "\n")
 
 		_ = cmdArgs
 
@@ -130,7 +209,7 @@ func proxy(in net.Conn, redisAddr string) {
 	outWriter := bufio.NewWriter(out)
 	go inLoop(inReader, outWriter)
 
-	inWriter := bufio.NewWriter(out)
-	outReader := bufio.NewReader(in)
-	go outLoop(inWriter, outReader)
+	//	inWriter := bufio.NewWriter(out)
+	//	outReader := bufio.NewReader(in)
+	//	go outLoop(inWriter, outReader)
 }
